51c7d897f66f6aba331f7e343d5aff40e562bb3b
MstarClint <obiohaclinton111@gmail.com>
2023-11-14 19:52:54 -0800

    printf functions

--- utils.c
+++ utils.c
@@ -1,4 +1,4 @@
-#inckude "main.h"
+#include "main.h"
 
 /**
  * is_printable - Evaluates if a char is printable
@@ -84,4 +84,3 @@ long int convert_size_unsgnd(unsigned long int num, int size)
 
 	return ((unsigned int)num);
 }
-
